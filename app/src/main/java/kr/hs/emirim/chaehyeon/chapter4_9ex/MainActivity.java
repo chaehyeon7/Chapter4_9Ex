89c0d16410ea25b0a1eb171e4f5c6d25ff73fcd4
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-05-04 20:42:00 +0900

    4_9번 finish

--- app/src/main/java/kr/hs/emirim/chaehyeon/chapter4_9ex/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/chapter4_9ex/MainActivity.java
@@ -3,12 +3,31 @@ package kr.hs.emirim.chaehyeon.chapter4_9ex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgv;
+    int angle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnRotate = findViewById(R.id.btn_retate);
+        imgv = findViewById(R.id.imgv);
+
+        btnRotate.setOnClickListener(btnListener);
+
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            angle += 10;
+            imgv.setRotation(angle);
+        }
+    };
 }
